582d37e4d40ddb0b8a2560bce6b1dc3f7c98cf63
Priyank <priyankdhameliya56@gmail.com>
2022-03-31 23:29:37 +0530

    Added contents for scroller

--- src/components/widgets/ContentScroller/index.jsx
+++ src/components/widgets/ContentScroller/index.jsx
@@ -15,9 +15,17 @@ const ContentScroller = () => {
             </h2>
           </div>
         </div>
-        <div className="row scroller-sec">
-          <div className="col-md-6 mb-5"></div>
-          <div className="col-md-6 w-50 mb-5">
+        <div className="row scroller-sec gx-5">
+          <div className="col-lg-6 col-md-10 mb-5 p-5 text-start">
+            <h3>Organize whiteboards lectures</h3>
+            <p className="text-muted">
+              ACV content blocks make creating beautiful online documentation
+              simple. Format text as headers, to-do lists, images, code snippets
+              and more – or just embed existing content seamlessly into your
+              notes.
+            </p>
+          </div>
+          <div className="col-lg-6 col-md-10 mb-5">
             <div className="block1 rounded-5 overflow-hidden">
               <StaticImage
                 src="/assets/images/contents/img1.png"
@@ -25,14 +33,34 @@ const ContentScroller = () => {
               />
             </div>
           </div>
-          <div className="col-md-6 w-50 mb-5">
+        </div>
+        <div className="row scroller-sec gx-5">
+          <div className="col-lg-6 col-md-10 mb-5 mt-5">
             <div className="block2 rounded-5 mb-5 overflow-visible">
               <StaticImage src="/assets/images/contents/img2.png" />
             </div>
           </div>
-          <div className="col-md-6"></div>
-          <div className="col-md-6 mb-5"></div>
-          <div className="col-md-6 w-50 mb-5">
+          <div className="col-lg-6 col-md-10 mb-5 p-5 mt-5 text-start">
+            <h3>Organize whiteboards lectures</h3>
+            <p className="text-muted">
+              ACV content blocks make creating beautiful online documentation
+              simple. Format text as headers, to-do lists, images, code snippets
+              and more – or just embed existing content seamlessly into your
+              notes.
+            </p>
+          </div>
+        </div>
+        <div className="row scroller-sec gx-5">
+          <div className="col-lg-6 col-md-10 mb-5 p-5 text-start">
+            <h3>Organize whiteboards lectures</h3>
+            <p className="text-muted">
+              ACV content blocks make creating beautiful online documentation
+              simple. Format text as headers, to-do lists, images, code snippets
+              and more – or just embed existing content seamlessly into your
+              notes.
+            </p>
+          </div>
+          <div className="col-lg-6 col-md-10 mb-5">
             <div className="block3 rounded-5 overflow-hidden">
               <StaticImage
                 src="/assets/images/contents/img3.png"
