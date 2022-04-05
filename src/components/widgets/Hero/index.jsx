9c00610c5cf79bf86baf95dbac4fc1cfe936b4fc
Priyank <priyankdhameliya56@gmail.com>
2022-04-05 23:52:59 +0530

    netlify bug

--- src/components/widgets/Hero/index.jsx
+++ src/components/widgets/Hero/index.jsx
@@ -6,7 +6,7 @@ import './Hero.css';
 
 const Hero = () => {
   return (
-    <div className="hero space-x">
+    <div className="space-x">
       <div className="container">
         <MainHeading />
         <CompareSwiper />
